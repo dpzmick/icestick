896db5ee80148cd975c3db17c0c46e732f0ee0be
David Zmick <dpzmick@gmail.com>
2019-04-03 00:26:59 -0500

    sent _something_

--- spi/tb/spi_simple.cpp
+++ spi/tb/spi_simple.cpp
@@ -94,7 +94,7 @@ TEST_CASE("slave does nothing when not selected", "[spi]")
 
   auto sim = SimBuilder<>().add(m).get_sim();
 
-  s->SSEL = 0;
+  s->SSEL = 0; // FIXME actually selection works differently than this
 
   auto prev_miso = s->MISO;
   for (size_t i = 0; i < 100; ++i) {
@@ -106,7 +106,6 @@ TEST_CASE("slave does nothing when not selected", "[spi]")
 }
 
 namespace t1 {
-  // need to consider how to do large sends, buffering somewhere?
   struct Master {
     MAKE_STATE(SendingClockUp);
     MAKE_STATE(SendingClockDown);
@@ -115,7 +114,7 @@ namespace t1 {
     Events transition(Uninitialized, InitEvent) {
       s->SSEL = 1; // select the spi module
 
-      curr_bit = 32; // msb first
+      curr_bit = 8; // msb first
       state = SendingClockDown{}; // next timeout will trigger a rising edge
       return Only{Timeout{10}};
     }
@@ -146,9 +145,9 @@ namespace t1 {
       , value(value)
     { }
 
-    spi*     s;
-    uint32_t value;
-    size_t   curr_bit;
+    spi*    s;
+    uint8_t value;
+    size_t  curr_bit;
 
     // state machine junk
     auto currentState() const { return state; }
@@ -156,36 +155,43 @@ namespace t1 {
   };
 
   struct Slave {
-    MAKE_STATE(WaitingForData); // told module to post a result somewhere
-    MAKE_STATE(Done);           // got a result
+    MAKE_STATE(WaitingForData);  // told module to post a result somewhere
+    MAKE_STATE(WaitingForClock); // waiting for fpga posedge
+    MAKE_STATE(Done);            // got a result
 
     Events transition(Uninitialized, InitEvent) {
-      static_assert(sizeof(s->out) == sizeof(uint32_t), "!");
+      static_assert(sizeof(s->out) == sizeof(uint8_t), "!");
 
-      CHECK(s->out_ready == 0);
+      CHECK(s->out_avail == 0);
       state = WaitingForData{};
       return OneOf{
-        RisingEdge{&(s->out_ready)}, // it worked
+        RisingEdge{&(s->out_avail)}, // it worked
         Timeout{1000}                // it failed
       };
     }
 
     Events transition(WaitingForData, RisingEdge) {
+      state = WaitingForClock{};
+      return Only{
+        Timeout{1}  // FIXME hack
+      };
+    }
+
+    Events transition(WaitingForClock, Timeout) {
       REQUIRE(s->out == value);
       state = Done{};
       done = true;
       return None{};
     }
 
-    template <typename Any>
-    Events transition(Any, Timeout) {
+    Events transition(WaitingForData, Timeout) {
       REQUIRE(!(bool)("timeout")); // test failed, we timedout
       return None{};
     }
 
-    spi*     s;
-    uint32_t value;
-    bool&    done;
+    spi*    s;
+    uint8_t value;
+    bool&   done;
 
     Slave(spi* s, uint32_t value, bool& done)
       : s(s)
@@ -195,7 +201,7 @@ namespace t1 {
 
     // state machine junk
     auto currentState() const { return state; }
-    States<WaitingForData, Done> state;
+    States<WaitingForData, WaitingForClock, Done> state;
   };
 } // namespace t1
 
@@ -204,8 +210,136 @@ TEST_CASE("master -> slave, single", "[spi]")
   std::unique_ptr<spi> s(new spi);
   bool                 done(false);
   VMachine<spi>        m(s.get(), done, 2);
-  t1::Master           master(s.get(), 54321);
-  t1::Slave            slave(s.get(), 54321, done);
+  t1::Master           master(s.get(), 111);
+  t1::Slave            slave(s.get(), 111, done);
+
+  auto sim = SimBuilder<>().add(m).add(master).add(slave).get_sim();
+  while (!done) sim.poll();
+}
+
+namespace t2 {
+  struct Master {
+    MAKE_STATE(Waiting); // Waiting for the magic sequence to show up
+    MAKE_STATE(Done);    // success
+
+    Events transition(Uninitialized, InitEvent) {
+      s->SSEL = 1; // select the spi module
+      value   = 0; // reset our local cached value
+
+      // slave will start sending "eventually", but we need to keep toggling the
+      // clock and consuming bytes
+      state = Waiting{};
+      return AllOf{
+        Timeout{10,   0}, // retrigger the clock
+        Timeout{1000, 1}, // fail the test
+      };
+    }
+
+    Events transition(Waiting, Timeout t) {
+      if (t.user_id == 1) {
+        REQUIRE(!(bool)"timeout");
+        return None{}; // we are all done, this is unreachable
+      }
+
+      s->SCK = ~s->SCK;
+      if (s->SCK) { // only on rising edge
+        value = (value << 1) | (uint8_t)s->MISO;
+      }
+
+      // uint8_t n = value;
+      // while (n) {
+      //   if (n & 1)
+      //     printf("1");
+      //   else
+      //     printf("0");
+
+      //   n >>= 1;
+      // }
+      // printf("\n");
+
+      if (value == magic) {
+        state = Done{};
+        done  = true;
+        return None{};
+      }
+      else {
+        state = Waiting{};
+        return Only{Timeout{10}};
+      }
+    }
+
+    Events transition(Done, Timeout) {
+      return None{}; // already done, ignore the timeout
+    }
+
+    Master(spi* s, uint8_t magic, bool& done)
+      : s(s)
+      , magic(magic)
+      , done(done)
+    { }
+
+    spi*    s;
+    uint8_t value;
+    uint8_t magic;
+    bool&   done;
+
+    // state machine junk
+    auto currentState() const { return state; }
+    States<Waiting, Done> state;
+  };
+
+  struct Slave {
+    MAKE_STATE(WaitingForSend);
+    MAKE_STATE(Sent);
+    MAKE_STATE(Done);
+
+    Events transition(Uninitialized, InitEvent) {
+      static_assert(sizeof(s->out) == sizeof(uint8_t), "!");
+
+      state = WaitingForSend{};
+      return OneOf{
+        RisingEdge{&(s->send_avail)}, // it worked
+        Timeout{1000}                 // it failed
+      };
+    }
+
+    Events transition(WaitingForSend, RisingEdge) {
+      // we send in a single tick of the FPGA clock (then let SPI do its thing)
+      s->in      = value;
+      s->send_in = 1;
+
+      state = Sent{};
+      return Only{Timeout{4}}; // need to wait for clock to go up and down again
+    }
+
+    Events transition(Sent, Timeout) {
+      s->send_in = 0; // all done
+
+      state = Done{};
+      return None{};
+    }
+
+    spi*    s;
+    uint8_t value;
+
+    Slave(spi* s, uint32_t value)
+      : s(s)
+      , value(value)
+    { }
+
+    // state machine junk
+    auto currentState() const { return state; }
+    States<WaitingForSend, Sent, Done> state;
+  };
+} // namespace t1
+
+TEST_CASE("slave -> master, single", "[spi]")
+{
+  std::unique_ptr<spi> s(new spi);
+  bool                 done(false);
+  VMachine<spi>        m(s.get(), done, 2);
+  t2::Master           master(s.get(), 111, done);
+  t2::Slave            slave(s.get(), 111);
 
   auto sim = SimBuilder<>().add(m).add(master).add(slave).get_sim();
   while (!done) sim.poll();
